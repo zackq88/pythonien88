045b4c1c3f8f966ddb5989c2451278bb016b9ec6
zackq88 <zakaria_elhabib.bouzit@g.enp.edu.dz>
2020-11-19 21:41:39 +0100

    Create opencv.py

---
+++ opencv.py
@@ -0,0 +1,10 @@
+cv2.imread()=img 
+cv2.imshow()
+cv2.waitkey(0)
+img.resize()
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)-->to gray
+cap=cv2.VideoCapture()
+cv2.putText(..)
+
+cap=cv2.VideoCapture("..")
+While True:
